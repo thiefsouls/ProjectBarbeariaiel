01241dbd9e7a754effdddcd97e56dba2c796a3e6
thiefsouls <93953129+thiefsouls@users.noreply.github.com>
2024-01-10 14:23:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function enviar_formulario (){
+    var submit = document.querySelector("#checkbox");
+
+    
+        var nome = document.querySelector('#nome-completo').value;
+        var email = document.querySelector('#email').value;
+        var telefone = document.querySelector("#telefone").value;
+        var mensagem = document.querySelector("#mensagem").value;
+    
+        console.log(nome, email, telefone, mensagem);
+    
+}
+
+
+
